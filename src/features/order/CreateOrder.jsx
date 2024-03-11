b2528a9c4ccd9259b690454d66e9c7b8cceaf8ee
minimomo14 <93943633+minimomo14@users.noreply.github.com>
2024-03-11 17:30:58 -0400

    create form errors handling

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { await, Form, redirect } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation} from "react-router-dom";
 import {createOrder} from "../../services/apiRestaurant";
 
 
@@ -34,6 +34,11 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const navigation = useNavigation()
+  const isSubmitting = navigation.state === "submitting";
+
+  const formErrors = useActionData();
+
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
@@ -52,6 +57,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -73,7 +79,8 @@ function CreateOrder() {
         </div>
 
         <div>
-          <button>Order now</button>
+        <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <button disabled={isSubmitting}> {isSubmitting ? "Placing order... " : "Order now"}</button>
         </div>
       </Form>
     </div>
@@ -90,6 +97,15 @@ export async function action({request}) {
     priority: data.priority === "on",
   };
 
+
+ const errors = {}
+  if(!isValidPhone(order.phone)) 
+    errors.phone = 
+    "Please enter the correct phone number.";
+  if(Object.keys(errors).length > 0 ) return errors;
+
+
+  // if everything is good then create new order and redirect.
   const newOrder = await createOrder(order);
   // console.log(data);
 
